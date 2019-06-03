25030f770d6b1ca990d0f848c6d9fc871c54f04a
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-03 17:11:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,6 +83,11 @@ def adding_matz
         }
      }
 
+  programmer_hash = :yukihiro_matsumoto => {
+     :known_for => "Ruby",
+     :languages => ["LISP", "C"]
+   }
+
   programmer_hash[:yukihiro_matsumoto] = {
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
